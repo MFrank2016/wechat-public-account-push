81a1832fa2dc97343b4d2af68a7efa2e949ba996
Frank <qiuyingjia2019@gmail.com>
2023-06-20 08:11:55 +0800

    add log

--- config/exp-config.js
+++ config/exp-config.js
@@ -2,5 +2,5 @@ import USER_CONFIG from './index.cjs'
 
 // const config = process.env.USER_INFO ? /* istanbul ignore next */ JSON.parse(process.env.USER_INFO) : USER_CONFIG
 const config = USER_CONFIG
-console.log(`获取config >>> ${config}`)
+console.log("获取config >>>" +  JSON.stringify(config))
 export default config
