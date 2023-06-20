365205c58acc76eb1cdde10026796ff9fc0ccf6b
Frank <qiuyingjia2019@gmail.com>
2023-06-20 08:43:10 +0800

    change temp

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '徐小宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozGdD6ZcCHkySrYUA0R8J0igcy8I',
+      id: 'ozGdD6RnPPRzWHD4UHLLtYW_hcBY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'PKXoBnarSF6qkR150wx-Ge0J0Ie7r0GxdTtHdilnTbM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -34,7 +34,10 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1995', date: '06-25',
+          type: '*生日', name: '徐小宝', year: '1995', date: '06-25',
+        },
+        {
+          type: '*生日', name: '邱大宝', year: '1995', date: '05-20',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
