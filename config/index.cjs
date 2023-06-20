03705be54e2dfcd5ac7b47592518a53252e26efb
Frank <qiuyingjia2019@gmail.com>
2023-06-20 08:39:49 +0800

    change temp

--- config/index.cjs
+++ config/index.cjs
@@ -14,42 +14,37 @@ const USER_CONFIG = {
   APP_SECRET: '9f68a2972d61033810401a9853832758',
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '咸阳',
+  PROVINCE: '湖南',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '长武',
+  CITY: '娄底',
 
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '徐小宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ozGdD6ZcCHkySrYUA0R8J0igcy8I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ArutKaS-xw6lOblYdpR_qmjzyFVfAXcwFJIqbiUwP6k',
+      useTemplateId: 'PKXoBnarSF6qkR150wx-Ge0J0Ie7r0GxdTtHdilnTbM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-25',
       province: '湖南',
       city: '娄底',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1995', date: '06-25',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '02-18',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-07-27' },
       ],
     },
   ],
